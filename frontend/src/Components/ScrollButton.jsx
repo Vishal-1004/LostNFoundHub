b91df2c68ccacac2b524fa917785df0fe5845b5d
Surya <musurya2014@gmail.com>
2023-11-16 14:58:05 +0530

    Added a Scroll Button to Home Page

---
+++ frontend/src/Components/ScrollButton.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect,useState } from "react";
+
+
+function BackToTop(){
+
+
+    const [backToTop,setBackToTop] = useState(false);
+
+
+    useEffect(()=>{
+            window.addEventListener("scroll",()=>{
+                if (window.scrollY>100){
+                    setBackToTop(true);
+                } else{
+                    setBackToTop(false);
+                }
+            })
+    },[])
+
+    const scrollUp = ()=>{
+        window.scrollTo({
+            top: 0,
+            behavior:"smooth"
+        })
+    }
+
+  return(<div>
+      {backToTop&& (
+            <button style={{
+                position:"fixed",
+                bottom:"50px",
+                right:"20px",
+                background:"transparent",
+                border:"none",
+            }} onClick={scrollUp}> <svg xmlns="http://www.w3.org/2000/svg" position="fixed"  width="45" height="45" fill="Blue" className="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+          </svg> </button>
+        )}
+  </div>)
+}
+
+export default BackToTop
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Components/Hero";
 import ContactUs from "../Components/ContactUs";
+import BackToTop from "../Components/ScrollButton";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <ContactUs />
+      <BackToTop/>
     </>
   );
 };
