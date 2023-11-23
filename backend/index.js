948b6c856e581d0cd33956bc4179ae4f12b0e195
Adithya <adithya3012@gmail.com>
2023-11-23 21:17:28 +0530

    Get user data by id added

--- backend/index.js
+++ backend/index.js
@@ -64,6 +64,23 @@ app.post("/login", async (req, res) => {
 
 // url: /profile/:id
 
+app.get("/profile/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ user: user });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Some Error Occurred", error: error.message });
+  }
+});
+
 app.get("/", async (req, res) => {
   try {
     const allUserData = await User.find();
