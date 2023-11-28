91c63578bd36bfba143478186c7fb533fcc5e52b
Surya <musurya2014@gmail.com>
2023-11-28 12:34:48 +0530

    Made Policy Page Responsive

--- frontend/src/Pages/Policy.jsx
+++ frontend/src/Pages/Policy.jsx
@@ -4,65 +4,67 @@ import styled from 'styled-components';
 function PrivacyPolicy() {
   return (
     <PolicyStyle>
-      <div className='container'>
-        <div className="main-heading">
-          Privacy Policy
-        </div>
-        <div className="p">
-          <ul>
-            <li>
-              <h2>1. Information Collected</h2>
+      <div className="row ps-5 ">
+        <div className="col-12 col-md-8 col-lg-12">
+          <div className="main-heading">Privacy Policy</div>
+          <div className="p">
+            <div>
               <ul>
                 <li>
-                  <h3>1.1. The Website collects user-provided information during registration.</h3>
+                  <h2>1. Information Collected</h2>
+                  <ul>
+                    <li>
+                      <h3>1.1. The Website collects user-provided information during registration.</h3>
+                    </li>
+                    <li>
+                      <h3>1.2. Lost and Found item information posted by users is also stored.</h3>
+                    </li>
+                  </ul>
                 </li>
                 <li>
-                  <h3>1.2. Lost and Found item information posted by users is also stored.</h3>
+                  <h2>2. Use of Information</h2>
+                  <ul>
+                    <li>
+                      <h3>2.1. User information is used for account management and communication.</h3>
+                    </li>
+                    <li>
+                      <h3>2.2. Lost and Found item information is displayed on the respective pages.</h3>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <h2>3. Sharing of Information</h2>
+                  <ul>
+                    <li>
+                      <h3>3.1. User information is not shared with third parties without consent.</h3>
+                    </li>
+                    <li>
+                      <h3>3.2. Found item information may be shared with the claimant.</h3>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <h2>4. Security</h2>
+                  <ul>
+                    <li>
+                      <h3>4.1. The Website employs security measures to protect user information.</h3>
+                    </li>
+                    <li>
+                      <h3>4.2. Users are responsible for maintaining the security of their login credentials.</h3>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <h2>5. Cookies</h2>
+                  The Website may use cookies to enhance user experience.
+                </li>
+                <li>
+                  <h2>6. Changes to Privacy Policy</h2>
+                  The Privacy Policy may be updated without notice. Users are advised to review it regularly.
                 </li>
               </ul>
-            </li>
-            <li>
-              <h2>2. Use of Information</h2>
-              <ul>
-                <li>
-                  <h3>2.1. User information is used for account management and communication.</h3>
-                </li>
-                <li>
-                  <h3>2.2. Lost and Found item information is displayed on the respective pages.</h3>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <h2>3. Sharing of Information</h2>
-              <ul>
-                <li>
-                  <h3>3.1. User information is not shared with third parties without consent.</h3>
-                </li>
-                <li>
-                  <h3>3.2. Found item information may be shared with the claimant.</h3>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <h2>4. Security</h2>
-              <ul>
-                <li>
-                  <h3>4.1. The Website employs security measures to protect user information.</h3>
-                </li>
-                <li>
-                  <h3>4.2. Users are responsible for maintaining the security of their login credentials.</h3>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <h2>5. Cookies</h2>
-              The Website may use cookies to enhance user experience.
-            </li>
-            <li>
-              <h2>6. Changes to Privacy Policy</h2>
-              The Privacy Policy may be updated without notice. Users are advised to review it regularly.
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </div>
     </PolicyStyle>
@@ -77,24 +79,25 @@ const PolicyStyle = styled.section`
   }
 
   .main-heading {
-    display: inline-block;
-    margin-top: 2rem !important;
+    display: inline;
+    margin-top: 1rem !important;
     font-size: 2rem;
     font-weight: 400 !important;
     border-bottom: 3px solid #0d6efd;
-    text-align:center;
-    margin-left:36rem !important;
+    margin-left: 38rem !important;
+    
   }
 
   ul {
     list-style-type: none;
     padding: 0;
-    padding-left: 55px;
+    padding-left: 15px;
     padding-top: 20px;
   }
 
   li {
-    margin-bottom: 1.5rem;
+    margin-bottom: 1rem;
+    overflow-wrap: break-word;
   }
 
   h2 {
@@ -106,7 +109,26 @@ const PolicyStyle = styled.section`
     font-size: 1.1rem;
     color: #0d6efd;
     padding-left: 24px;
-    color: #333;    
+    color: #333;
+  }
+
+  /* Media Queries for Responsive Design */
+  @media (max-width: 767px) {
+    .main-heading {
+      margin-left: 69px !important;
+      text-align: center;
+      font-size: 1.4rem;
+    }
+
+    h2{
+        padding-left: 0px;
+        margin-left: -30px;
+        font-size: 1.2rem;
+    }
+    h3{
+        padding-right: 10px;
+        font-size: 1rem;
+    }
   }
 `;
 
